6ca10370d156e65bff677fa9f4739234ec623f3a
Dmytro Grunytsky <dima.grunitsky@gmail.com>
2020-10-13 23:15:55 +0300

    Добуток

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A");
+            double numbera = double.Parse(Console.ReadLine());
+            Console.WriteLine("B");
+            double numberb = double.Parse(Console.ReadLine());
+            double summaAB = numbera * numberb;
+            Console.WriteLine($"summa{summaAB}");
+
+            
+        }
+    }
+}
